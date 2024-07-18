9a47a9aa6c28ced05d0c35a4cc2765fbc1ac75b3
sohilaehab25 <sohailaehab25@gmail.com>
2024-07-18 14:42:09 +0200

    make a prototype function

--- scripts.js
+++ scripts.js
@@ -2,7 +2,22 @@
 const Person = function(fullName, bornYear){
     this.fullName = fullName;
     this.bornYear= bornYear;
+
+    //we can not make methods there becouse when we make the inistance of the constractor it must inhirit the method and we do not need the method in every object so we make prototype
 }
 
+//prototype function
+Person.prototype.calcAge = function(){
+    console.log(2024-this.bornYear);
+
+};
+
 const sohila = new Person('sohila', 2000)
-console.log(sohila)
+
+console.log(sohila);
+
+sohila.calcAge();
+
+Person.prototype.isPrototypeOf(Person) //false
+Person.prototype.isPrototypeOf(sohila) //true
+
